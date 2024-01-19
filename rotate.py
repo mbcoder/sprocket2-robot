00697bd2cea54997048b86d54ad4f43180f4b287
mbcoder <mbaird@esri.com>
2024-01-19 14:24:07 +0000

    robotics...

--- rotate.py
+++ rotate.py
@@ -10,10 +10,35 @@ print(angle)
 
 stepsNeeded = abs(int((steps360 / 360) * angle))
 
+# setup gpio
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(config.enablePin, GPIO.OUT)
+GPIO.setup(config.directionPinLeft, GPIO.OUT)
+GPIO.setup(config.directionPinRight, GPIO.OUT)
+GPIO.setup(config.pulsePin, GPIO.OUT)
+
+# enable stepper
+GPIO.output(config.enablePin, GPIO.HIGH)
+
+# work out direction of rotation (+ve = clockwise)
 if angle > 0:
     print("clockwise")
+    GPIO.output(config.directionPinLeft, GPIO.LOW)
+    GPIO.output(config.directionPinRight, GPIO.LOW)
 else:
     print("anti cw")
+    GPIO.output(config.directionPinLeft, GPIO.HIGH)
+    GPIO.output(config.directionPinRight, GPIO.HIGH)
 
 print(stepsNeeded)
-# work out direction of rotation (+ve = clockwise)
+
+# loop
+for steps in range(stepsNeeded):
+    GPIO.output(config.pulsePin, GPIO.HIGH)
+    sleep(0.004)
+    GPIO.output(config.pulsePin, GPIO.LOW)
+    sleep(0.004)
+
+# disable stepper
+GPIO.output(config.enablePin, GPIO.LOW)

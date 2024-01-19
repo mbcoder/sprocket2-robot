4f477feeae330c4dc5031331ebb8167c6998590c
mbcoder <mbaird@esri.com>
2024-01-19 14:16:25 +0000

    robotics...

--- rotate.py
+++ rotate.py
@@ -3,11 +3,22 @@ import RPi.GPIO as GPIO
 from time import sleep
 import config
 
-stepsPerDegree = 5
+steps360 = 2281
 
 angle = int(sys.argv[1])
 print(angle)
 
+stepsNeeded = int((steps360/360)*angle)
+
+print(stepsNeeded)
+
+if angle>0:
+    print("clockwise")
+else:
+    print("anti cw")
+
+
+print(stepsNeeded)
 # work out direction of rotation (+ve = clockwise)
 
 

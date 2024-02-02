4b43eac849919cc821fbd6ed1371498c1335ad07
David R. Smith <davidsmith@esri.com>
2024-02-02 15:29:36 +0000

    Use python-sim commands

--- src/main/java/com/mycompany/app/CommandRunner.java
+++ src/main/java/com/mycompany/app/CommandRunner.java
@@ -19,6 +19,8 @@ package com.mycompany.app;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import com.mycompany.sockets.pojos.HttpRequest;
@@ -40,47 +42,54 @@ public class CommandRunner {
    * @return a response String to send back (will be sent via the socket)
    */
   public String execCommand() {
+    String responseBody;
 
-    // These will be replaced by a python command
-    final String[] validForwardCommand = {"bash",  "-c", "echo Success with forward"};
-    final String[] invalidForwardCommand = {"bash",  "-c", "echo 'Failed (25)'"};
-    final String[] validRotateCommand = {"bash",  "-c", "echo Success with rotation"};
+    final List<String> commandList = new ArrayList<>();
+    commandList.add("python");
 
     try {
       // Expecting a command URL like  https://127.0.0.1:8080/testOne?forward=100
-      // Split this on question mark to isolate the part like "forward=100"
-      // If the command string has 100000 then fail the command to test failure paths
-      String[] commands = request.getUri().toString().split("\\?");
-      if (commands.length >= 2) {
+      // Split this on question mark and equals to isolate the part like "forward=100"
+      String[] commands = request.getUri().toString().split("[?=]");
+      if (commands.length == 3) {
         String command = commands[1];
-        String [] execString;
-        if (command.contains("forward")) {
-          if (command.contains("100000")) {
-            execString = invalidForwardCommand;
+        String commandProcess = null;
+        String commandParameter = null;
+
+        if (command.equals("forward")) {
+          commandProcess = "forward-sim.py";
+        } else if (command.equals("rotate")) {
+          commandProcess = "rotate-sim.py";
+        }
+
+        if (commands[2].matches("^ *-?\\d+ *$")) {
+          commandParameter = commands[2];
+        }
+
+        if (commandProcess != null && commandParameter != null) {
+          commandList.add(commandProcess);
+          commandList.add(commandParameter);
+          System.out.println(commandList);
+          Process runExternalProcess = Runtime.getRuntime().exec(commandList.toArray(new String[0]));
+          if (!runExternalProcess.waitFor(1, TimeUnit.MINUTES)) {
+            responseBody = String.format("Timeout... %s", request.getUri().toString());
           } else {
-            execString = validForwardCommand;
+            // Get the stdout of the process - return the first line
+            try (var stdout = new BufferedReader(new InputStreamReader(runExternalProcess.getInputStream()))) {
+              responseBody = stdout.readLine();
+            }
           }
         } else {
-          execString = validRotateCommand;
+          responseBody = String.format("Error: %s", request.getUri().toString());
         }
-
-        Process runExternalProcess = Runtime.getRuntime().exec(execString);
-
-        if (!runExternalProcess.waitFor(1, TimeUnit.MINUTES)) {
-          return "FAILED external process waitFor problem";
-        }
-
-        // Get the stdout of the process
-        try (var stdout = new BufferedReader(new InputStreamReader(runExternalProcess.getInputStream()))) {
-          var line = stdout.readLine();
-          return String.format("<HTML> <P>%s</P> </HTML>", line);
-        }
+      } else {
+        responseBody = String.format("Error: %s", request.getUri().toString());
       }
-
-      return String.format("<HTML> <P> Invalid... %s</P> </HTML>", request.getUri().toString());
     } catch (IOException | InterruptedException e) {
-      System.out.println("exception");
-      return "FAILED " + e.getMessage();
+      responseBody = String.format("Exception... %s:%s", e.getMessage(), request.getUri().toString());
     }
+
+    System.out.println(responseBody);
+    return String.format("<HTML> <P>%s</P> </HTML>", responseBody);
   }
 }
